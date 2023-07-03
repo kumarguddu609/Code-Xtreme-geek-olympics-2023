c50bb14ffb594e58e97225f96e421c975b29512f
Guddu Kumar <kumarguddu609@gmail.com>
2023-07-03 22:21:24 +0530

    Update Maximum Index.java

--- Maximum Index.java
+++ Maximum Index.java
@@ -18,3 +18,42 @@ class Solution {
         return max_diff;
     }
 }
+
+
+//// Time Complexity-- O(n)
+//Space Complexity-- O(n)  //extra auxillary space
+class Solution {
+
+    int maxIndexDiff(int arr[], int n) {
+       int LMin[]=new int[n];
+       int RMax[]=new int[n];
+       
+       //Storing min from left in array
+       LMin[0]=arr[0];
+       for(int i=1;i<n;i++){
+           
+               LMin[i]=Math.min(arr[i],LMin[i-1]);
+           
+       }
+       
+        //Storing max from right in array
+        RMax[n-1]=arr[n-1];
+       for(int j=n-2;j>=0;--j){
+           RMax[j]=Math.max(arr[j],RMax[j+1]);
+       }
+       
+       int i=0; 
+       int j=0;
+       int max_diff=-1;
+       while(i<n &&j<n){
+           if(LMin[i]<=RMax[j]){
+               max_diff=Math.max(max_diff,j-i);
+               j++;
+           }
+           else{
+               i++;
+           }
+       }
+       return max_diff;
+    }
+}
