48358c2a387cf7bf6715a3c63b2f976ca0de49fb
Guddu Kumar <kumarguddu609@gmail.com>
2023-07-04 23:53:42 +0530

    Update Count the subarrays having product less than k.java

--- Count the subarrays having product less than k.java
+++ Count the subarrays having product less than k.java
@@ -20,3 +20,30 @@ class Solution {
         }
         
     }
+///
+//
+class Solution {
+    
+    public int countSubArrayProductLessThanK(long a[], int n, long k)
+    
+    { 
+       long p=1;
+        int ans=0,i=0,j=0;
+        for(i=0, j=0; j<n ; j++)
+        {
+            p = p*a[j];
+            while(p>=k && i<j)
+            {
+                p/=a[i];
+                i++;
+            }
+            
+            if(p<k)
+            ans+= (j-i+1);
+        
+        }
+        return ans;
+        
+        }
+        
+    }
