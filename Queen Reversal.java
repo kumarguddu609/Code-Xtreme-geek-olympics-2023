952ef2783c031ef0193f693c43e3669addaa193e
Guddu Kumar <kumarguddu609@gmail.com>
2023-07-16 17:18:27 +0530

    Create Queen Reversal.java

---
+++ Queen Reversal.java
@@ -0,0 +1,19 @@
+//POTD
+//
+//
+//
+//
+class GfG{
+    //Function to reverse the queue.
+    public Queue<Integer> rev(Queue<Integer> q){
+         Stack<Integer> st =  new Stack<>();
+        while(!q.isEmpty())
+        st.push(q.remove());
+        
+        while(!st.isEmpty())
+        q.add(st.pop());
+        
+        
+        return q;
+    }
+}
