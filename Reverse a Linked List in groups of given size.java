353b10b6755b2229a97bdf99d3cc9fd3ca0be25b
Guddu Kumar <kumarguddu609@gmail.com>
2023-07-21 19:35:13 +0530

    Create Reverse a Linked List in groups of given size.java

---
+++ Reverse a Linked List in groups of given size.java
@@ -0,0 +1,26 @@
+//POTD
+//
+//
+//
+//
+class Solution {
+    public static Node reverse(Node head, int k) {
+        if (head == null) return null;
+        
+        Node prev = null;
+        Node next = null;
+        Node headptr = head;
+        int cnt = k;
+        
+        while (headptr != null && cnt != 0) {
+            next = headptr.next;
+            headptr.next = prev;
+            prev = headptr;
+            headptr = next;
+            cnt--;
+        }
+        
+        head.next = reverse(headptr, k);
+        
+        return prev;
+    }
